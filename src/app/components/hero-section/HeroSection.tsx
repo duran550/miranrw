38a5217f007489a0161e88dae4861ba37b25ef4c
pierretoube97 <pierre.toube@kaeyros-analytics.com>
2023-09-21 13:09:50 +0100

    fix: remove Image from landing page

--- src/app/components/hero-section/HeroSection.tsx
+++ src/app/components/hero-section/HeroSection.tsx
@@ -38,11 +38,6 @@ const HeroSection: React.FC<heroSectionPropsType> = ({ lang, content }) => {
             </Button>
           </div>
 
-          {/* <Image
-            src={graphic}
-            className="md:w-[300px] lg:w-[600px] hidden md:block"
-            alt="graphic"
-          /> */}
         </div>
       </div>
     </>
