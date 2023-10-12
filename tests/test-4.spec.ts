d74c8078e60c028cb872d0bcc74acf4bcf4d68b0
Cyrille <cyrille.sepele@kaeyros-analytics.com>
2023-10-12 18:29:38 +0100

    ci: changed url in tests

--- tests/test-4.spec.ts
+++ tests/test-4.spec.ts
@@ -1,28 +1,65 @@
 import { test, expect } from '@playwright/test';
+import { url } from './site';
 
 test('test', async ({ page }) => {
-  test.slow()
-  await page.goto('http://localhost:3000/en');
+  test.slow();
+  await page.goto(`${url}/en`);
   await page.getByRole('link', { name: 'Report an Incident' }).click();
-  await page.getByRole('button', { name: 'Let\'s start' }).click();
+  await page.getByRole('button', { name: "Let's start" }).click();
   await page.getByLabel('An organization/institution').check();
-  await page.locator('div').filter({ hasText: /^Next$/ }).first().click();
+  await page
+    .locator('div')
+    .filter({ hasText: /^Next$/ })
+    .first()
+    .click();
   await page.getByLabel('Cultural Institution').check();
   await page.getByLabel('Association').check();
-  await page.locator('div').filter({ hasText: /^Next$/ }).first().click();
+  await page
+    .locator('div')
+    .filter({ hasText: /^Next$/ })
+    .first()
+    .click();
   await page.getByLabel('10-49').check();
-  await page.locator('div').filter({ hasText: /^Next$/ }).first().click();
+  await page
+    .locator('div')
+    .filter({ hasText: /^Next$/ })
+    .first()
+    .click();
   await page.getByLabel('It happened online').check();
-  await page.locator('div').filter({ hasText: /^Next$/ }).first().click();
+  await page
+    .locator('div')
+    .filter({ hasText: /^Next$/ })
+    .first()
+    .click();
   await page.getByLabel('Romantic Orientation').check();
   await page.getByLabel('Body').check();
-  await page.locator('div').filter({ hasText: /^Next$/ }).first().click();
+  await page
+    .locator('div')
+    .filter({ hasText: /^Next$/ })
+    .first()
+    .click();
   await page.getByLabel('Homophobia').check();
   await page.getByLabel('Agender discrimination').check();
-  await page.locator('div').filter({ hasText: /^Next$/ }).first().click();
+  await page
+    .locator('div')
+    .filter({ hasText: /^Next$/ })
+    .first()
+    .click();
   await page.getByLabel('No').check();
-  await page.locator('div').filter({ hasText: /^Next$/ }).first().click();
-  await page.getByLabel('I consent to sharing my information with the relevant reporting agencies in case of multiple discrimination, in an anonymized form (hyperlink to the list).').check();
-  await page.getByLabel('I hereby confirm the accuracy of my information, acknowledge the data protection regulations (hyperlink to data protection concept), and consent to the described processing of my personal data.*').check();
+  await page
+    .locator('div')
+    .filter({ hasText: /^Next$/ })
+    .first()
+    .click();
+  await page
+    .getByLabel(
+      'I consent to sharing my information with the relevant reporting agencies in case of multiple discrimination, in an anonymized form (hyperlink to the list).'
+    )
+    .check();
+  await page
+    .getByLabel(
+      'I hereby confirm the accuracy of my information, acknowledge the data protection regulations (hyperlink to data protection concept), and consent to the described processing of my personal data.*'
+    )
+    .check();
   await page.getByRole('button', { name: 'Submit' }).click();
 });
