35715152655b0034f2164a0734e2ae0f52eb0988
Cyrille <cyrille.sepele@kaeyros-analytics.com>
2024-03-20 18:08:16 +0100

    fix: fixed linting warning

--- src/app/[lang]/(dashboard)/dashboard/quantitative/loading.tsx
+++ src/app/[lang]/(dashboard)/dashboard/quantitative/loading.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 const Loading = () => {
   return (
     <div className="w-full h-[90vh] flex flex-col gap-y-5">
-      <div className="h-24 rounded-xl w-full bg-slate-100 animate-pulse"></div>
-      <div className="h-full rounded-xl w-full bg-slate-100 animate-pulse"></div>
+      <div className="h-24 rounded-xl w-full bg-slate-300 animate-pulse"></div>
+      <div className="h-full rounded-xl w-full bg-slate-300 animate-pulse"></div>
     </div>
   );
 };
