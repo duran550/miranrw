7ef7f194e4152043376e00a72da64d809219ab5d
ivans-wandji <ivans.wandji@kaeyros-analytics.com>
2024-03-21 15:57:12 +0100

    feat: redirect the reports

--- src/app/[lang]/(dashboard)/common/components/reports/ReportSingle.tsx
+++ src/app/[lang]/(dashboard)/common/components/reports/ReportSingle.tsx
@@ -3,15 +3,18 @@ import React from 'react';
 import Header from './Header';
 import ReportSummary from './reports-cleaner/report-summary/ReportSummary';
 import ReportActions from './reports-cleaner/report-actions/ReportActions';
+import { useAuth } from '@/app/hooks/useAuth';
 
 const ReportSingle = () => {
+  const { user }=useAuth()
   return (
     <div>
       <Header href="/cleaned-data" title="Data Info" />
 
       <div className="flex  gap-x-6">
         <ReportSummary />
-        <ReportActions />
+
+        {user && user.role === 3 && <ReportActions />}
       </div>
     </div>
   );
