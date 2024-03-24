626837b20b4315cd9782481bd0983b6928e587fb
Cyrille <cyrille.sepele@kaeyros-analytics.com>
2024-03-24 07:49:54 +0100

    feat: fixed reports risk manager

--- src/app/[lang]/(dashboard)/common/components/reports/ReportsRiskManager.tsx
+++ src/app/[lang]/(dashboard)/common/components/reports/ReportsRiskManager.tsx
@@ -1,7 +1,71 @@
-import React from 'react';
+'use client';
+import ReportContainCard from '@/app/components/dashboard/reports/ReportContainCard';
+import React, { useState } from 'react';
+
+import { Button } from '@/app/components/button/Button';
+import imgcatActive from '../../../../../../../public/images/Checkmark Starburst (1).svg';
+import imgcatDesactive from '../../../../../../../public/images/Checkmark Starburst (1).svg';
+import imgUncatDesactive from '../../../../../../../public/images/Square Dismiss.svg';
+import imgUncatActive from '../../../../../../../public/images/Square Dismiss (1).svg';
+import {
+  reportsCardTableCategorized,
+  reportsCardTableUncategorized,
+} from '../../../dashboard/reports/reportsCardDatas';
+import { Category } from '../report-card/reportCard.d';
 
 const ReportsRiskManager = () => {
-  return <div>ReportsRiskManager</div>;
+  const [status, setStatut] = useState(Category.Dangerous);
+  return (
+    <div className="w-full relative  h-fit">
+      <h1 className="text-2xl font-bold my-8">All reports</h1>
+      <h2 className="font-bold  opacity-80">{`${status} Data`}</h2>
+      <p className="text-sm opacity-70">Click to view data details</p>
+      <div className="mt-8">
+        {status == Category.Dangerous ? (
+          <ReportContainCard
+            href="/dashboard/cleaned-reports"
+            data={reportsCardTableUncategorized}
+          />
+        ) : (
+          <ReportContainCard
+            href="/dashboard/cleaned-reports"
+            data={reportsCardTableCategorized}
+          />
+        )}
+      </div>
+
+      <div className="flex w-fit fixed bottom-8  mt-14 ">
+        <Button
+          icon={
+            status == Category.Dangerous ? imgUncatActive : imgUncatDesactive
+          }
+          className={`w-auto ${
+            status == Category.Dangerous
+              ? 'bg-black rounded-xl text-white font-semibold'
+              : 'text-[#828B8C]  bg-transparent'
+          }`}
+          onClick={() => {
+            setStatut(Category.Dangerous);
+          }}
+        >
+          {Category.Dangerous}
+        </Button>
+        <Button
+          icon={status == Category.Managed ? imgcatActive : imgcatDesactive}
+          className={`w-auto ${
+            status == Category.Managed
+              ? 'bg-black rounded-xl text-white font-semibold'
+              : 'text-[#828B8C] bg-transparent'
+          }`}
+          onClick={() => {
+            setStatut(Category.Managed);
+          }}
+        >
+          {Category.Managed}
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default ReportsRiskManager;
