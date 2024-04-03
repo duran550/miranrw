b8cb019932de0da8649aec440bf56d847e2c27c3
duran550 <138498476+duran550@users.noreply.github.com>
2024-04-03 18:15:39 +0100

    feat: settings report number fixed

--- src/app/[lang]/(dashboard)/common/components/forms/radio/Checkbox.tsx
+++ src/app/[lang]/(dashboard)/common/components/forms/radio/Checkbox.tsx
@@ -43,11 +43,11 @@ const Checkbox: React.FC<CheckboxProps> = ({
           >
             <polygon
               points="456.851,0 245,212.564 33.149,0 0.708,32.337 212.669,245.004 0.708,457.678 33.149,490 245,277.443 456.851,490 
-	489.292,457.678 277.331,245.004 489.292,32.337 "
+	489.292,457.678 277.331,245.004 489.292,32.337"
             />
           </svg>
         </div>
-        <label htmlFor={id} className="select-none ">
+        <label htmlFor={id} className="select-none">
           {label}
         </label>
       </div>
