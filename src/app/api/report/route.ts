8a88473a7b9eb40abfe8db9c0e60bacb0e8c6c08
ivans-wandji <ivans.wandji@kaeyros-analytics.com>
2024-04-03 17:05:18 +0100

    feat: decomment

--- src/app/api/report/route.ts
+++ src/app/api/report/route.ts
@@ -19,8 +19,8 @@ export async function POST(request: any) {
 export async function GET(request: any) {
   let pass= await rateLimitMiddleware(request)
   if (!pass) return NextResponse.json({ status: 'Error', message: 'Too Many Requests.' }, { status: 400 });
-  // let flag = await authenticate(request)
-  // if (!flag) return NextResponse.json({ status: 'Error', message: 'Access Denied. Invalid Token.' }, { status: 400 });
+  let flag = await authenticate(request)
+  if (!flag) return NextResponse.json({ status: 'Error', message: 'Access Denied. Invalid Token.' }, { status: 400 });
   await dbConnect();
   let reports: reportType[] = await Report.find().populate('updatereport');
   return NextResponse.json({ reports });
