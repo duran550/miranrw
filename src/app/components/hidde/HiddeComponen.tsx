5b738f8e0eea3fd7356762be2018bd315ecdc4a5
ivans-wandji <ivans.wandji@kaeyros-analytics.com>
2024-06-20 13:13:49 +0100

    feat: test

--- src/app/components/hidde/HiddeComponen.tsx
+++ src/app/components/hidde/HiddeComponen.tsx
@@ -8,24 +8,22 @@ const HiddeComponen = () => {
   const [code, setCode] = useState('');
   const pat = usePathname();
   useEffect(() => {
-    if (code.length == 10 && code == 'at#4|IJj%2') {
+    if (code == 'mi*$at#4|IJjQar%2') {
       setShow('true');
-      window.location.href =  '/';
+      window.location.href = '/';
     }
   }, [code]);
   return (
-    <div className="flex flex-col justify-center items-center p-5 gap-3">
-      <h1 className="font-bold lg:text-xl">
-        Put the code of 10 characters here to access the website
-      </h1>
+    <div className="flex flex-col justify-center items-center p-5 gap-3 sm:w-[500px] w-full">
+      <h1 className="font-bold lg:text-xl text-center">Password</h1>
       <input
         type="text"
-        className="h-14 md:w-full w-96 border-2 rounded-xl ms:text-sm  px-5 text-center"
+        className="h-14 w-full border-2 rounded-xl ms:text-sm  px-5 text-center"
         onChange={(e: any) => {
           setCode(e.target.value);
         }}
       />
-      {code.length > 0 && code != 'at#4|IJj%2' && (
+      {code.length > 0 && code != 'mi*$at#4|IJjQar%2' && (
         <p className="text-red-900 text-sm">code incorrect</p>
       )}
     </div>
